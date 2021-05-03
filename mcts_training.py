aeddb0b8f1dc10db7fc008a2da37792459c89ad3
NeiH4207 <NeiH4207@gmail.com>
2021-05-04 02:59:21 +0700

    update model

--- mcts_training.py
+++ mcts_training.py
@@ -37,8 +37,8 @@ args = dotdict({
     'colab_train': False,
     'colab_dir': "/content/drive/MyDrive/trainned_model/agent_mcts.pt",
     'checkpoint': './temp/',
-    'load_model': False,
-    'load_folder_file': ('Models','model.pt'),
+    'load_model': True,
+    'load_folder_file': ('Models','agent_mcts.pt'),
     'numItersForTrainExamplesHistory': 15,
     'saved_model': True
 })
